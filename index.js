63f59735a91d014519ae08675369d37c3f568a4f
gloriosorenz <38281715+gloriosorenz@users.noreply.github.com>
2023-02-12 00:32:10 +0800

    add flag

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ document.querySelector('.img2').setAttribute("src", image2);
 
 // Winner logic
 if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 Wins";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins";
 }
 else if(randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 Wins";
+    document.querySelector("h1").innerHTML = "Player 2 Wins 🚩";
 }
 else{
     document.querySelector("h1").innerHTML = "Draw";
